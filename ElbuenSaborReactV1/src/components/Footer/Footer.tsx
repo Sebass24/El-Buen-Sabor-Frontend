e541014aa249c4890b658ab72e3ba770b6e35930
Emilia Chiofalo <emichiofalo@gmail.com>
2023-05-11 10:51:23 -0300

    responsive footer

--- ElbuenSaborReactV1/src/components/Footer/Footer.tsx
+++ ElbuenSaborReactV1/src/components/Footer/Footer.tsx
@@ -1,41 +1,40 @@
-import React from "react";
 import "./Footer.scss";
 import Logo from "components/NavBar/Logo/Logo";
 
 const Footer = () => {
   return (
-    <div>
-      <div className="Container_Footer">
-        <div className="Container_Footer_contact">
-          <span className="Contactanos">Contactanos</span>
-          <div className="Container_Footer_contact_items">
-            <i className="fa-solid fa-location-dot"></i>
-            <span>Mendoza-Argentina</span>
-          </div>
-          <div className="Container_Footer_contact_items">
-            <i className="fa-brands fa-whatsapp"></i>
-            <span>+54-9-2616172242</span>
-          </div>
-          <div className="Container_Footer_contact_items">
-            <i className="fa-solid fa-envelope"></i>
-            <span>ElBuenSaborMendoza@gmail.com</span>
-          </div>
+    <div className="Container_Footer">
+      
+      <div className="Container_Footer_contact">
+        <span><label>Contactanos</label></span>
+        <div className="Container_Footer_contact_items">
+          <i className="fa-solid fa-location-dot"></i>
+          <label>Mendoza-Argentina</label>
         </div>
-
-        <Logo className="footer-logo"/>
-
-        <div className="PaymentMethods">
-          <span className="Contactanos">Métodos de pago</span>
-          <div className="Container_cards">
-            <i className="fa-brands fa-cc-mastercard"></i>
-            <i className="fa-brands fa-cc-visa"></i>
-            <i className="fa-brands fa-cc-paypal"></i>
-            <i className="fa-brands fa-cc-amex"></i>
-            <img src="/mercadopago.png"></img>
-            <i className="fa-brands fa-cc-apple-pay"></i>
-          </div>
+        <div className="Container_Footer_contact_items">
+          <i className="fa-brands fa-whatsapp"></i>
+          <label>+54-9-2616172242</label>
+        </div>
+        <div className="Container_Footer_contact_items">
+          <i className="fa-solid fa-envelope"></i>
+          <label>ElBuenSaborMendoza@gmail.com</label>
         </div>
       </div>
+
+      <Logo />
+
+      <div className="PaymentMethods">
+        <label>Métodos de pago</label>
+        <div className="Container_cards">
+          <i className="fa-brands fa-cc-mastercard"></i>
+          <i className="fa-brands fa-cc-visa"></i>
+          <i className="fa-brands fa-cc-paypal"></i>
+          <i className="fa-brands fa-cc-amex"></i>
+          <img src="/mercadopago.png"></img>
+          <i className="fa-brands fa-cc-apple-pay"></i>
+        </div>
+      </div>
+
     </div>
   );
 };
