d4e1a0cfb85745e00cc332c0064edf1d78245471
Emilia Chiofalo <emichiofalo@gmail.com>
2023-05-11 10:00:32 -0300

    responsive product cards and logo adjustment

--- ElbuenSaborReactV1/src/components/Footer/Footer.tsx
+++ ElbuenSaborReactV1/src/components/Footer/Footer.tsx
@@ -1,32 +1,30 @@
 import React from "react";
 import "./Footer.scss";
+import Logo from "components/NavBar/Logo/Logo";
+
 const Footer = () => {
   return (
     <div>
       <div className="Container_Footer">
-        <div className="Container_Foter_contact">
+        <div className="Container_Footer_contact">
           <span className="Contactanos">Contactanos</span>
-          <div className="Container_Foter_contact_items">
+          <div className="Container_Footer_contact_items">
             <i className="fa-solid fa-location-dot"></i>
             <span>Mendoza-Argentina</span>
           </div>
-          <div className="Container_Foter_contact_items">
+          <div className="Container_Footer_contact_items">
             <i className="fa-brands fa-whatsapp"></i>
             <span>+54-9-2616172242</span>
           </div>
-          <div className="Container_Foter_contact_items">
+          <div className="Container_Footer_contact_items">
             <i className="fa-solid fa-envelope"></i>
             <span>ElBuenSaborMendoza@gmail.com</span>
           </div>
         </div>
 
-        <div className="Arow_container_Footer">
-          <div className="arrowBottom_Footer">
-            <div className="imgLogo_Footer"></div>
-          </div>
-        </div>
+        <Logo className="footer-logo"/>
 
-        <div className="PayMethods">
+        <div className="PaymentMethods">
           <span className="Contactanos">Métodos de pago</span>
           <div className="Container_cards">
             <i className="fa-brands fa-cc-mastercard"></i>
