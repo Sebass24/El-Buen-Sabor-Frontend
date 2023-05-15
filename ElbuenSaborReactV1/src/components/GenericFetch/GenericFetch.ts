0fc3ef597c0d53fa0eb2e3e3ea35909b405eb73e
francoGonzalez27 <franco.gonzalez.reale.276@gmail.com>
2023-05-15 16:30:57 -0300

    Inicio Generic Fetchs

---
+++ ElbuenSaborReactV1/src/components/GenericFetch/GenericFetch.ts
@@ -0,0 +1,28 @@
+export async function getData<T>(path: string): Promise<T> {
+  const response = await fetch(`http://localhost:8080${path}`);
+  if (!response.ok) {
+    throw Error(response.statusText);
+  }
+  return response.json();
+}
+
+//si el metodo es post, data no tiene que tener id
+//si el metodo es put, data tiene que tener id
+async function postPutData<T>(
+  path: string,
+  method: string,
+  data: T
+): Promise<T> {
+  const response = await fetch(`http://localhost:8080${path}`, {
+    method: method.toUpperCase(),
+    headers: {
+      Accept: "application/json",
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(data),
+  });
+  if (!response.ok) {
+    throw Error(response.statusText);
+  }
+  return response.json();
+}
--- ElbuenSaborReactV1/src/components/Stock/Ingredientes/Ingredients.tsx
+++ ElbuenSaborReactV1/src/components/Stock/Ingredientes/Ingredients.tsx
@@ -1,53 +1,66 @@
-import React, { useState } from 'react';
-import "./Ingredients.scss"
-import { Button } from 'react-bootstrap';
+import React, { useEffect, useState } from "react";
+import "./Ingredients.scss";
+import { Button } from "react-bootstrap";
 import TableIngredients from "./TableIngredients/TableIngredients";
-import ModalAddIngrediente from './ModalAddIngrediente/ModalAddIngrediente';
-import { Ingredient } from '@Models/types';
-import ModalBuyIngredient from './ModalBuyIngredient/ModalBuyIngredient';
+import ModalAddIngrediente from "./ModalAddIngrediente/ModalAddIngrediente";
+import { Ingredient } from "@Models/types";
+import ModalBuyIngredient from "./ModalBuyIngredient/ModalBuyIngredient";
+import { getData } from "../../GenericFetch/GenericFetch";
 
 const Ingredients = () => {
-
   const [showModal, setShowModal] = useState(false);
   const handleClose = () => {
-    setShowModal(false)
-  }
+    setShowModal(false);
+  };
 
   const [showModalBuy, setShowModalBuy] = useState(false);
   const handleCloseBuy = () => {
-    setShowModalBuy(false)
-  }
-  const ingredientesPrueba: Ingredient[] = [{
-    Nombre: "salsa",
-    Rubro: "Salsas",
-    PrecioCosto: 340,
-    StockMinimo: 2,
-    StockActual: 5,
-    UnidadMedida: "cm3",
-    NivelStock: "Optimo",
-    Estado: "Baja"
-  },
-  {
-    Nombre: "pepino",
-    Rubro: "Verduras",
-    PrecioCosto: 500,
-    StockMinimo: 2,
-    StockActual: 5,
-    UnidadMedida: "cm3",
-    NivelStock: "Optimo",
-    Estado: "Alta"
-  },]
+    setShowModalBuy(false);
+  };
 
-  const [ingredient, setIngredient] = useState<Ingredient[]>(ingredientesPrueba);
-  const [ingredientComplete, setIngredientComplete] = useState<Ingredient[]>(ingredientesPrueba);
+  // const ingredientesPrueba: Ingredient[] = [
+  //   {
+  //     Nombre: "salsa",
+  //     Rubro: "Salsas",
+  //     PrecioCosto: 340,
+  //     StockMinimo: 2,
+  //     StockActual: 5,
+  //     UnidadMedida: "cm3",
+  //     NivelStock: "Optimo",
+  //     Estado: "Baja",
+  //   },
+  //   {
+  //     Nombre: "pepino",
+  //     Rubro: "Verduras",
+  //     PrecioCosto: 500,
+  //     StockMinimo: 2,
+  //     StockActual: 5,
+  //     UnidadMedida: "cm3",
+  //     NivelStock: "Optimo",
+  //     Estado: "Alta",
+  //   },
+  // ];
+
+  const [ingredient, setIngredient] = useState<Ingredient[]>([]);
+  const [ingredientComplete, setIngredientComplete] = useState<Ingredient[]>(
+    []
+  );
   const [search, setSearch] = useState("");
 
+  async function getIngredients() {
+    const data: Ingredient[] = await getData<Ingredient[]>("/api/ingredient");
+    setIngredient(data);
+    setIngredientComplete(data);
+  }
+
+  useEffect(() => {
+    getIngredients();
+  }, []);
 
   const handleChange = (e: any) => {
     setSearch(e.target.value);
     filter(e.target.value);
   };
-
 
   const filter = (serchParam: string) => {
     var serchResult = ingredientComplete.filter((ingredientVal: Ingredient) => {
@@ -82,17 +95,20 @@ const Ingredients = () => {
     setIngredient(serchResult);
   };
 
-
   return (
-    <div className='Container_Ingredientes' >
-      <div className='actions_Ingredientes'>
-        <div className='actions_Ingredientes_buttons'>
-          <Button variant="warning" onClick={() => setShowModalBuy(true)} >Registrar Compra</Button>
-          <Button variant="success" onClick={() => setShowModal(true)}>Nuevo</Button>
+    <div className="Container_Ingredientes">
+      <div className="actions_Ingredientes">
+        <div className="actions_Ingredientes_buttons">
+          <Button variant="warning" onClick={() => setShowModalBuy(true)}>
+            Registrar Compra
+          </Button>
+          <Button variant="success" onClick={() => setShowModal(true)}>
+            Nuevo
+          </Button>
         </div>
         <div>
           <span>Nivel de stock: </span>
-          <select className='Select_nivelStock'>
+          <select className="Select_nivelStock">
             <option>Todos</option>
             <option>Faltante</option>
             <option>Optimo</option>
@@ -100,17 +116,24 @@ const Ingredients = () => {
           </select>
         </div>
         <div className="Container_input">
-          <input placeholder="Busqueda" className="busqueda_comida" value={search} onChange={handleChange}></input>
-          <i className="fa-solid fa-magnifying-glass" style={{ color: "black" }}></i>
+          <input
+            placeholder="Busqueda"
+            className="busqueda_comida"
+            value={search}
+            onKeyUp={(event) => {
+              if (event.key === "Enter") {
+                handleChange(event);
+              }
+            }}
+          ></input>
+          <i
+            className="fa-solid fa-magnifying-glass"
+            style={{ color: "black" }}
+          ></i>
         </div>
       </div>
-      <TableIngredients
-        Ingredients={ingredient}
-      />
-      <ModalAddIngrediente
-        showModal={showModal}
-        handleClose={handleClose}
-      />
+      <TableIngredients Ingredients={ingredient} />
+      <ModalAddIngrediente showModal={showModal} handleClose={handleClose} />
 
       <ModalBuyIngredient
         handleClose={handleCloseBuy}
@@ -118,10 +141,8 @@ const Ingredients = () => {
         handleCloseNew={handleClose}
         setShowModalNew={setShowModal}
       />
-
     </div>
-
   );
-}
+};
 
 export default Ingredients;
