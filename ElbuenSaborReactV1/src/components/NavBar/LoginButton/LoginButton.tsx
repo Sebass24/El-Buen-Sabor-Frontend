237192acfebdb85f7ef40ef1541c23c3ee235891
Emilia Chiofalo <emichiofalo@gmail.com>
2023-06-17 09:32:42 -0300

    refactor admin bar
    deleted duplicate loginbutton, used loginbutton component instead

--- ElbuenSaborReactV1/src/components/NavBar/LoginButton/LoginButton.tsx
+++ ElbuenSaborReactV1/src/components/NavBar/LoginButton/LoginButton.tsx
@@ -12,7 +12,7 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import "./LoginButton.scss";
 import { useState } from "react";
 import PersonalDataModal from "components/Users/UsersPersonalData/PersonalDataModal";
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 import { useAppSelector } from "@app/Hooks";
 
 library.add(faUser);
@@ -26,6 +26,8 @@ export const LoginButton = () => {
     setShowPersonalData(false);
   }
 
+  const navigate = useNavigate();
+
   return (
     <div className="Container_RightNavBar">
       {isAuthenticated ?
@@ -35,21 +37,25 @@ export const LoginButton = () => {
             <FontAwesomeIcon icon={faUser} />
           </Dropdown.Toggle>
           <Dropdown.Menu className="login-button">
-            <Dropdown.Item onClick={() => { setShowPersonalData(true) }}>Mis datos personales</Dropdown.Item>
+            <Dropdown.Item onClick={() => { setShowPersonalData(true) }}><span>Mis datos personales</span></Dropdown.Item>
             {user.role?.description === "Administrador" ? (<>
-              <Dropdown.Item ><Link to="/cashier" className="link"><span>Cajero</span></Link></Dropdown.Item>
-              <Dropdown.Item ><Link to="/cook" className="link"><span>Cocinero</span></Link></Dropdown.Item>
-              <Dropdown.Item ><Link to="/delivery" className="link"><span>Delivery</span></Link></Dropdown.Item>
-              <Dropdown.Item ><Link to="/admin" className="link"><span>Administrador</span></Link></Dropdown.Item>
+              <Dropdown.Item onClick={() => { navigate("/home") }}><span>Home</span></Dropdown.Item>
+              <Dropdown.Item onClick={() => { navigate("/cashier") }}><span>Cajero</span></Dropdown.Item>
+              <Dropdown.Item onClick={() => { navigate("/cook") }}><span>Cocinero</span></Dropdown.Item>
+              <Dropdown.Item onClick={() => { navigate("/delivery") }}><span>Delivery</span></Dropdown.Item>
+              <Dropdown.Item onClick={() => { navigate("/admin") }}><span>Administrador</span></Dropdown.Item>
             </>) : <></>}
-            {user.role?.description === "Cocinero" ? (
-              <Dropdown.Item ><Link to="/cook" className="link"><span>Cocinero</span></Link></Dropdown.Item>
+            {user.role?.description === "Cocinero" ? (<>
+              <Dropdown.Item onClick={() => { navigate("/cook") }}><span>Cocinero</span></Dropdown.Item>
+            </>
             ) : <></>}
-            {user.role?.description === "Cajero" ? (
-              <Dropdown.Item ><Link to="/cashier" className="link"><span>Cajero</span></Link></Dropdown.Item>
+            {user.role?.description === "Cajero" ? (<>
+              <Dropdown.Item onClick={() => { navigate("/cashier") }}><span>Cajero</span></Dropdown.Item>
+            </>
             ) : <></>}
-            {user.role?.description === "Delivery" ? (
-              <Dropdown.Item ><Link to="/delivery" className="link"><span>Delivery</span></Link></Dropdown.Item>
+            {user.role?.description === "Delivery" ? (<>
+              <Dropdown.Item onClick={() => { navigate("/delivery") }}><span>Delivery</span></Dropdown.Item>
+            </>
             ) : <></>}
             <Dropdown.Item><LogOutAuth /></Dropdown.Item>
           </Dropdown.Menu>
